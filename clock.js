cb73d510a1b77b683f0435de1af69382e1ba2859
krutikpatel15 <krutikpatel02@gmail.com>
2021-01-22 14:18:59 +0530

    My project

---
+++ clock.js
@@ -0,0 +1,23 @@
+setInterval(myClock,1000);
+
+const hourHand = document.querySelector('[data-hour]');
+const minuteHand = document.querySelector('[data-minute]');
+const secondHand = document.querySelector('[data-second]');
+
+function myClock(){
+    const currentDate = new Date();
+
+    const secondsRatio = currentDate.getSeconds()/60;
+    const minutesRatio = (secondsRatio + currentDate.getMinutes())/60;
+    const hoursRatio = (minutesRatio + currentDate.getHours())/12;
+
+    setRotation(secondHand,secondsRatio);
+    setRotation(minuteHand,minutesRatio);
+    setRotation(hourHand,hoursRatio);
+}
+
+setRotation = (element,rotationRatio) =>{
+    element.style.setProperty('--rotation',rotationRatio*360);
+}
+
+myClock();
